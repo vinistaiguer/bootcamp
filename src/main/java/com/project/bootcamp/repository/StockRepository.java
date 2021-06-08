a04534bb65193ae30eed5e3f43f1057e1501b5f7
Vinicius Staiguer <viniciusstaiguer@hotmail.com>
2021-06-08 00:34:11 -0300

    alteração

--- src/main/java/com/project/bootcamp/repository/StockRepository.java
+++ src/main/java/com/project/bootcamp/repository/StockRepository.java
@@ -1,7 +1,6 @@
 package com.project.bootcamp.repository;
 
 import com.project.bootcamp.model.Stock;
-import com.project.bootcamp.model.dto.StockDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
